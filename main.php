f60c7b7d037e32be56f412414b4626f68474cc41
Ubuntu <maddie.madlad@gmail.com>
2022-06-29 16:09:22 +0200

    made use of getopts, created file loader funct

--- main.php
+++ main.php
@@ -1,6 +1,17 @@
 <?php
-if(!isset($argv[1])) die("File was not specified\n");
-$myfile = fopen($argv[1], "r") or die("Unable to open file\n");
-$j = json_decode(fread($myfile,filesize($argv[1])));
-var_dump($j);
-fclose($myfile);
+function loadData(string $path): Array{
+    $file = fopen($path, "r");
+    if(!$file) throw new Exception("Unable to open file $path\n");
+    $json = json_decode(fread($file,filesize($path)));
+    fclose($file);
+    return $json;
+}
+$flags = getopt("t:l:");
+if(!isset($flags['t'])||!isset($flags['l'])) die("File was not specified\n");
+try{
+    $tree = loadData($flags['t']);
+    $list = loadData($flags['l']);
+}catch(Exception $e){
+    echo $e->getMessage();
+    die();
+}
